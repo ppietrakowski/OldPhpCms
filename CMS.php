2bd31a44a429a8a2592c77619cdb8ec0cd738e14
pawelp <80946292+ppietrakowski@users.noreply.github.com>
2022-12-16 20:36:05 +0100

    Some refactoring

--- CMS.php
+++ CMS.php
@@ -1,127 +1,163 @@
 <html>
-    <head>
-        <meta charset='utf-8'>
-        <link href='style.css' rel='stylesheet'>
-    </head>
-    <body>
-        <h1><a href='CMS.php' class="header">CMS</a></h1>
-        <?php
-            class Cms {
-                public function __construct() {
-                    $this->connect();
-                }
-                public function showTable() {          
-                    echo "<table>";
-                    echo "
+
+<head>
+    <meta charset='utf-8'>
+    <link href='style.css' rel='stylesheet'>
+</head>
+
+<body>
+    <h1><a href='CMS.php' class="header">CMS</a></h1>
+    <?php
+    class Cms
+    {
+        public function __construct()
+        {
+            $this->Connection = new mysqli("localhost", "root", "", "mine");
+            $this->recordsCount = 0;
+
+            if ($this->MustAddRecord()) {
+                $this->TryAddRecord();
+            }
+
+            if (isset($_GET['edit'])) {
+                $this->EditRecord();
+            }
+
+            if (isset($_GET['delete'])) {
+                $this->DeleteRecord();
+            }
+        }
+
+        public function ShowTable()
+        {
+            echo "<table>";
+            $this->ShowHeaders();
+
+            $r = $this->Connection->query("SELECT * FROM osoby");
+            $this->recordsCount = 0;
+
+            while ($obj = mysqli_fetch_object($r)) {
+                $this->AddNewPersonField($obj);
+            }
+
+            echo "</table>";
+
+            // przekieruj sterowanie w zaleznosci od tego
+            // czy ma usunac czy edytowac czy dodac
+            if (isset($_GET['edit'])) {
+                $this->ShowEditForm();
+            } else {
+                echo "<form action='cms.php' method='get'>
+                                <input name='name' placeholder='imię'>
+                                <input name='surname' placeholder='nazwisko'>
+                                <input type='submit' value='Dodaj !'>
+                            </form>";
+            }
+        }
+
+        private function ShowHeaders()
+        {
+            echo "
                             <tr>
                                 <td> nr </td>
                                 <td> imie </td>
                                 <td> nazwisko </td>
                             </tr>
                         ";
-                    
-                    $r = $this->connection_->query("SELECT * FROM osoby");
-                    $number = 1;
-                    while($obj = mysqli_fetch_object($r)) {
-                        echo "
-                            <tr>
-                                <td>$number</td>
-                                <td>$obj->Name</td>
-                                <td>$obj->Surname</td>
-                                <td><a href='cms.php?edit=".$obj->Id."'>Edytuj</a></td>
-                                <td><a href='cms.php?delete=".$obj->Id."'>Usuń</a></td>
-                            </tr>
-                        ";
-                        $number++;
-                    }
-                    echo "</table>";
-                    // przekieruj sterowanie w zaleznosci od tego
-                    // czy ma usunac czy edytowac czy dodac
-                    $this->showForm();
-                    $this->addRecord();
-                    $this->editRecord();
-                    $this->deleteRecord();
-                }
-                private function connect() {
-                    // nawiazuje polaczenie
-                    $this->connection_ = new mysqli("localhost", "root", "", "mine");
-                }
-                private function showForm() {
-                    // pokazuje formularz
-                    // jezeli wybrano edytuj to wyswietli
-                    // formularz do edytowania
-                    // w przeciwnym(domyslnym) wypadku napisze
-                    // formularz do dodawania
-                    if(isset($_GET['edit'])) {
-                        $q = "SELECT * FROM osoby WHERE Id=".$_GET['edit'];
-                        $r = $this->connection_->query($q);
-                        $obj = mysqli_fetch_object($r);
-                        echo "<form action='cms.php' method='get'>
-                                <input name='name' value='".$obj->Name."'>
-                                <input name='surname' value='".$obj->Surname."'>
-                                <input name='edit' type='hidden' value=".$_GET['edit'].">
-                                <input type='submit' value='Edytuj'>
-                            </form>
-                        ";
-                    }
-                    else {
-                         
-                            echo "<form action='cms.php' method='get'>
-                                <input name='name' placeholder='imię'>
-                                <input name='surname' placeholder='nazwisko'>
-                                <input type='submit' value='Dodaj !'>
-                            </form>
-                        ";
-                    }
-                }
-                private function addRecord() {
-                    // sprawdza, czy powstaly trzy zmienne
-                    // za pomoca metody get
-                    if(isset($_GET['name']) && isset($_GET['surname']) && !(isset($_GET['edit']))) {
-                        if($_GET['name'] != '' && $_GET['surname'] != '') {
-                            // wybranie w celu nie wpisania tego
-                            // samego rekordu
-                            $q = "SELECT * FROM osoby WHERE Name='".$_GET['name']."' AND Surname='".$_GET['surname']."'";
-                            $r = $this->connection_->query($q);
-                            // nie ma zastrzezenia do przypisania
-                            if(($obj = mysqli_fetch_object($r)) == null) {
-                                $q = "INSERT INTO osoby(Name, Surname) VALUES('".$_GET['name']."', '".$_GET['surname']."')";
-                                $r = $this->connection_->query($q);
-                                
-                                // wyswietlanie komunikatow
-                                echo "<span class='ok'>Dodano rekord !</span>";
-                            }
-                            else {
-                                echo "<span class='error'>taki rekord już istnieje !</span>";
-                            }
-                        }
-                        else {
-                            echo "<span class='error'>nie dodam pustego rekordu !</span>";
-                        }
-                    } 
-                }
-                private function editRecord() {
-                    if(isset($_GET['edit'])) {
-                        if(isset($_GET['name']) && isset($_GET['surname'])) {
-                            $q = "UPDATE osoby SET Name='".$_GET['name']."', Surname='".$_GET['surname']."' WHERE Id=".$_GET['edit'];
-                            $r = $this->connection_->query($q);
-                            echo "<span class='ok'>Zmieniono rekord !</span>";
-                        }
-                    }
-                }
-                private function deleteRecord() {
-                    if(isset($_GET['delete'])) {      
-                            $q = "DELETE FROM osoby WHERE Id=".$_GET['delete'];
-                            $r = $this->connection_->query($q);
-                            echo "<span class='ok'>Usunięto rekord!</span>";
-                    }
-                }
-                private $connection_;
+        }
+
+        private function AddNewPersonField($obj)
+        {
+            echo "
+                    <tr>
+                        <td>$this->recordsCount</td>
+                        <td>$obj->Name</td>
+                        <td>$obj->Surname</td>
+                        <td><a href='cms.php?edit=" . $obj->Id . "'>Edytuj</a></td>
+                        <td><a href='cms.php?delete=" . $obj->Id . "'>Usuń</a></td>
+                    </tr>
+                    ";
+            $this->recordsCount++;
+        }
+
+        private function ShowEditForm()
+        {
+            $q = "SELECT * FROM osoby WHERE Id=" . $_GET['edit'];
+            $r = $this->Connection->query($q);
+            $obj = mysqli_fetch_object($r);
+            echo "<form action='cms.php' method='get'>
+                            <input name='name' value='" . $obj->Name . "'>
+                            <input name='surname' value='" . $obj->Surname . "'>
+                            <input name='edit' type='hidden' value=" . $_GET['edit'] . ">
+                            <input type='submit' value='Edytuj'>
+                        </form>
+                    ";
+        }
+
+        private function MustAddRecord()
+        {
+            // sprawdza, czy powstaly trzy zmienne
+            // za pomoca metody get
+            return isset($_GET['name']) && isset($_GET['surname']) && !(isset($_GET['edit']));
+        }
+
+        private function TryAddRecord()
+        {
+            if ($this->WasEnteredEmptyRecord()) {
+                $this->AddRecord();
+            } else {
+                echo "<span class='error'>Wykryto pusty rekord !</span>";
             }
-            ?>
-        <?php
-            $cms = new Cms();
-            $cms->showTable();
-        ?>
-    </body>
+        }
+
+        private function AddRecord()
+        {
+            // wybranie w celu nie wpisania tego
+            // samego rekordu
+            $q = "SELECT * FROM osoby WHERE Name='" . $_GET['name'] . "' AND Surname='" . $_GET['surname'] . "'";
+            $r = $this->Connection->query($q);
+
+            $isSameRecordAvailableInDatabase = mysqli_fetch_object($r) != null;
+
+            if (!$isSameRecordAvailableInDatabase) {
+                $q = "INSERT INTO osoby(Name, Surname) VALUES('" . $_GET['name'] . "', '" . $_GET['surname'] . "')";
+                $r = $this->Connection->query($q);
+
+                // wyswietlanie komunikatow
+                echo "<span class='ok'>Dodano rekord !</span>";
+            } else {
+                echo "<span class='error'>taki rekord już istnieje !</span>";
+            }
+        }
+
+        private function WasEnteredEmptyRecord()
+        {
+            return $_GET['name'] != '' && $_GET['surname'] != '';
+        }
+
+        private function EditRecord()
+        {
+            if (isset($_GET['name']) && isset($_GET['surname'])) {
+                $query = "UPDATE osoby SET Name='" . $_GET['name'] . "', Surname='" . $_GET['surname'] . "' WHERE Id=" . $_GET['edit'];
+                $this->Connection->query($query);
+                echo "<span class='ok'>Zmieniono rekord !</span>";
+            }
+        }
+
+        private function DeleteRecord()
+        {
+            $q = "DELETE FROM osoby WHERE Id=" . $_GET['delete'];
+            $this->Connection->query($q);
+            echo "<span class='ok'>Usunięto rekord!</span>";
+        }
+
+        private $Connection;
+    }
+    ?>
+    <?php
+    $cms = new Cms();
+    $cms->ShowTable();
+    ?>
+</body>
+
 </html>
